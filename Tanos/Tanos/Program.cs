32cdadd91fa5e2c369080417840ecd37b68e0392
Arshia <154830026+arshiashirzad@users.noreply.github.com>
2024-01-12 18:59:16 +0330

    adding the solved quera challenge

---
+++ Tanos/Tanos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tanos
+{   
+    using System.Collections.Generic;
+    internal class Tanos
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<string, string> rocks = new Dictionary<string, string>()
+            {
+                {"space","blue"},
+                {"time","green"},
+                {"mind","yellow"},
+                {"reality","red"}, 
+                {"power","purple"},
+                {"soul","orange"}
+            };
+            string rock= Console.ReadLine();
+            Console.WriteLine(rocks[rock]);
+        }
+    }
+}
