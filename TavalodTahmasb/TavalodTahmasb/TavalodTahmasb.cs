b884a437681bc8f0181c1bfaf2a33b2999ebca35
Arshia <154830026+arshiashirzad@users.noreply.github.com>
2024-01-12 22:26:02 +0330

    add solved quera challenge

---
+++ TavalodTahmasb/TavalodTahmasb/TavalodTahmasb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace TavalodTahmasb
+{
+    internal class TavalodTahmasb
+    {
+        public static void Main(string[] args)
+        {
+            int people = Convert.ToInt32(Console.ReadLine());
+            string[] degreeS = Console.ReadLine().Split(' ');
+            double[] myDegrees = Array.ConvertAll(degreeS, s => double.Parse(s));
+            Array.Sort(myDegrees);
+            double[] vals = new double[people];
+            for (int i = 0; i < myDegrees.Length; i++)
+            {
+                if (i + 1 == myDegrees.Length)
+                {
+                    vals[i] = (360-myDegrees[i]) + myDegrees[0];
+                }
+                else
+                {
+                    vals[i] = myDegrees[i + 1] - myDegrees[i];
+                }
+            }
+
+            
+            Console.WriteLine((vals.Max()/360)*100);
+        }
+    }
+}
