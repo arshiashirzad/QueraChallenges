1c0e261f602fb609f65b00b91f7f014ee3e92537
Arshia <154830026+arshiashirzad@users.noreply.github.com>
2024-01-12 19:37:59 +0330

    add solved quera challenge

---
+++ Marco/Marco/Marco.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Marco
+{
+    internal class Marco
+    {
+        public static void Main(string[] args)
+        {
+            int trips = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 1; i <= trips; i++)
+            {
+                int choices = Convert.ToInt32(Console.ReadLine());
+                int res=0;
+                for (int j = 1; j <= choices; j++)
+                { 
+                    string[] choice = Console.ReadLine().Split(' ');
+                    if (res == 0)
+                    {
+                        if (choice[0] == choice[1])
+                        {
+                            if (choice[0] == "buy_one")
+                            {
+                                res += 1;
+                            }
+                        }else {
+                            res += 1;
+                        }
+                    }else
+                    {
+                        if (choice[0] != choice[1])
+                        {
+                            res *=2;
+                        }
+                        else {
+                            if (choice[0] == "buy_one")
+                            {
+                                res += 1;
+                            }
+                            else
+                            {
+                                res *= 2;
+                            }
+                        }
+                    }
+
+                }
+                
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
