7cacdb44448ca21083276e9e595745eb45e87951
Arshia <154830026+arshiashirzad@users.noreply.github.com>
2024-05-17 13:14:46 +0330

    Solved!

---
+++ JadvalZarb/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int x ;
+    cin >> x;
+    for (int i =1 ; i<= x; i++ ){
+        for (int j = 1; j<= x ; j++){
+            cout<<(i*j)<<" ";
+        }
+        cout<<endl;
+    }
+return 0;
+}
