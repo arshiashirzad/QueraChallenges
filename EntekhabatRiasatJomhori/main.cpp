aad83185fb444f323fc533e1eaae7139cf629a6a
Arshia <154830026+arshiashirzad@users.noreply.github.com>
2024-05-27 18:59:56 +0330

    solved (Tricky)!

---
+++ EntekhabatRiasatJomhori/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void Josh(vector<int>& person, int k, int index) {
+    if (person.size() == 1) {
+        cout << person[0] << endl;
+        return;
+    }
+
+    index = (index + k) % person.size();
+    person.erase(person.begin() + index);
+
+    Josh(person, k, index);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int k = 1;
+    int index = 0;
+    vector<int> person;
+    for (int i = 1; i <= n; ++i) {
+        person.push_back(i);
+    }
+    Josh(person, k, index);
+
+    return 0;
+}
